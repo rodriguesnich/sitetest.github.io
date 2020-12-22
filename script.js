3a49201b77ec6f4e898981be3dd27d2c89ff9326
Daniel Costa <daniiel.cavallcante@gmail.com>
2020-12-22 15:40:38 -0300

    :wolf: Informações no Footer e texto sobre Slider de Vídeo

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+  
+$(document).ready(function () {
+    $(".sidenav").sidenav();
+});
+
+document.getElementById("myVideo").autoplay;
